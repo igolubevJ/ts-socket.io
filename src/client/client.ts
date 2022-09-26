06ce8f7a73680bd65ca3458c4c11f6f2603a10ce
igolubev <igolubevapp@gmail.com>
2022-09-26 16:03:27 +0300

    Server IO Emit

--- src/client/client.ts
+++ src/client/client.ts
@@ -9,6 +9,11 @@ class Client {
       console.log(message);
       document.body.innerHTML += message + '<br />';
     });
+
+    this.socket.on('random', function(message: any) {
+      console.log(message);
+      document.body.innerHTML = 'Winning number is ' + message + '<br />';
+    });
   }
 }
 
--- src/server/server.ts
+++ src/server/server.ts
@@ -31,6 +31,10 @@ class App {
         console.log('socket disconnect:', socket.id);
       });
     });
+
+    setInterval(() => {
+      this.io.emit('random', Math.floor(Math.random() * 10));
+    }, 1000);
   }
 
   public Start(): void {
